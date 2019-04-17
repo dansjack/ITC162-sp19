664c48b3cb8033e6564ea4e002458820d5f3382d
dsjack <dsjack89@gmail.com>
2019-04-16 20:07:12 -0700

    Adds a constant and ouputs a message to the log

--- w3app/app/src/main/java/com/example/w3app/MainActivity.java
+++ w3app/app/src/main/java/com/example/w3app/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.w3app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constant TAG
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "We are in the OnCreate method of MainActivity");
     }
 }
